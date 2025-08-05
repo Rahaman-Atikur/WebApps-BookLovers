b45d4374be4d41619c99ca48a17cfd9ca1526d4e
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-08-05 22:25:39 +0100

    Phase 2 done:Nav bar and Folder Organization done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,24 @@
+//Router Components Definition
+import React from 'react';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router";
+import Error from '../Pages/Error/Error';
+import Home from '../Pages/Home/Home';
+import Roots from '../Pages/Roots/Roots';
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        Component:Roots ,
+        errorElement: <Error></Error>,
+        children: [
+            {
+                index: true,
+                path: "/",
+                Component: Home
+            }
+        ]
+    },
+]);
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { router } from './Routes/Routes.jsx'
 import {
-  createBrowserRouter,
-  RouterProvider,
+    createBrowserRouter,
+    RouterProvider,
 } from "react-router";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <h1 className='text-5xl font-bold text-red-800'>Hello World</h1>,
-  },
-  {
-    path: "/About",
-    element: <h1 className='text-5xl font-bold text-red-800'>Hello About</h1>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 )
