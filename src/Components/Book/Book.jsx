eeae0a9729131e2624227fd6b4df96dfe76993a3
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-08-06 13:46:08 +0100

    Phase Five Done: React Icons in Card

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import { FaStar } from "react-icons/fa6";
 const Book = ({ singleBook }) => {
     console.log(singleBook);
     // console.log(singleBook.images);
-    const { bookName, author, image } = singleBook;
+    const { bookName, author, image, category, rating } = singleBook;
     return (
         <div className="  card bg-base-100 w-96 shadow-sm">
-            <figure>
+            <figure className='bg-gray-100'>
                 <img className='w-[320px] h-[280px]'
                     src={image}
                     alt="Shoes" />
@@ -18,8 +18,8 @@ const Book = ({ singleBook }) => {
                 </h2>
                 <p>{author}</p>
                 <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                    <div className="badge badge-outline">{category}</div>
+                    <div className="badge badge-outline">{rating} <FaStar /></div>
                 </div>
             </div>
         </div>
